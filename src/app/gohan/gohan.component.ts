17c98206558144ff4a306e34485901a391b8f4fd
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-07-11 18:57:35 -0400

    terminando proyecto

--- src/app/gohan/gohan.component.ts
+++ src/app/gohan/gohan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-gohan',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GohanComponent implements OnInit {
 
+  @Output()
+  emisor = new EventEmitter();
+
+  @Input()
+  ki: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  darEnergia() {
+    this.emisor.emit('TOMA MI ENERGIA!!!!');
   }
 
 }
